73ae3094dcc57741f1d0816aceab70a5f20a5419
LucMat50 <lmaturino5@gmail.com>
2026-03-18 12:36:37 -0700

    updated input map and added running

--- cs-4800-unnamed-game/player.gd
+++ cs-4800-unnamed-game/player.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
-@export var speed = 400
+const SPEED = 450
+const RUN_SPEED = 600
 
 func get_input():
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = input_direction * speed
+	if Input.is_action_pressed("run"):
+		velocity = input_direction * RUN_SPEED
+	else: 
+		velocity = input_direction * SPEED
 	
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	get_input()
 	move_and_slide()
