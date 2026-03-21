9871c9a6c82366202c095034aa62055c1b469087
LucMat50 <lmaturino5@gmail.com>
2026-03-21 11:57:12 -0700

    always chases

--- cs-4800-unnamed-game/scripts/enemy.gd
+++ cs-4800-unnamed-game/scripts/enemy.gd
@@ -11,19 +11,18 @@ const SPEED = 400
 @onready var player_node: CharacterBody2D = get_parent().get_node("Player")
 
 func _physics_process(delta: float) -> void:
-	if chase:
-		direction = (player_node.global_position - global_position).normalized()
-		velocity = lerp(velocity, direction * SPEED, 8.5 * delta)
-		move_and_slide()
+	direction = (player_node.global_position - global_position).normalized()
+	velocity = lerp(velocity, direction * SPEED, 8.5 * delta)
+	move_and_slide()
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body == player_node:
-		get_tree().call_deferred("reload_current_scene")
+#func _on_area_2d_body_entered(body: Node2D) -> void:
+#	if body == player_node:
+#		get_tree().call_deferred("reload_current_scene")
 
-func _on_enter_area_body_entered(body: Node2D) -> void:
-	if body == player_node:
-		chase = true
+#func _on_enter_area_body_entered(body: Node2D) -> void:
+#	if body == player_node:
+#		chase = true
 
-func _on_exit_area_body_exited(body: Node2D) -> void:
-	if body == player_node:
-		chase = false
+#func _on_exit_area_body_exited(body: Node2D) -> void:
+#	if body == player_node:
+#		chase = false
