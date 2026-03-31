218e6f17df1be57f05519d7f9dae599b1b394aff
LucMat50 <lmaturino5@gmail.com>
2026-03-30 20:08:07 -0700

    set groups added game manager script

--- cs-4800-unnamed-game/scripts/bullet.gd
+++ cs-4800-unnamed-game/scripts/bullet.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 # CONSTANTS
 var SPEED = 500
+
 # NORMAL VARIABLES
 var movement_vector = Vector2(0, -1)
 
@@ -10,3 +11,9 @@ var movement_vector = Vector2(0, -1)
 
 func _process(delta: float) -> void:
 	position += transform.x * SPEED * delta
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Enemy"):
+		var enemy = body
+		enemy.die()
+		queue_free()
--- cs-4800-unnamed-game/scripts/enemy.gd
+++ cs-4800-unnamed-game/scripts/enemy.gd
@@ -1,4 +1,4 @@
-extends CharacterBody2D
+class_name Enemy extends CharacterBody2D
 
 # NORMAL VARIABLES
 var chase = true
@@ -15,6 +15,9 @@ func _physics_process(delta: float) -> void:
 		direction = (player_node.global_position - global_position).normalized()
 		velocity = lerp(velocity, direction * SPEED, 8.5 * delta)
 		move_and_slide()
+
+func die():
+	queue_free()
 
 #func _on_area_2d_body_entered(body: Node2D) -> void:
 #	if body == player_node:
