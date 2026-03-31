6d79c93ed026b0d15b9807cb9698ff9ba5925ada
LucMat50 <lmaturino5@gmail.com>
2026-03-30 19:51:05 -0700

    gun now shoots

--- cs-4800-unnamed-game/scripts/bullet.gd
+++ cs-4800-unnamed-game/scripts/bullet.gd
@@ -1,14 +1,12 @@
 extends Node2D
 
+# CONSTANTS
+var SPEED = 500
 # NORMAL VARIABLES
-var speed = 700
+var movement_vector = Vector2(0, -1)
 
 # ONREADY VARIABLES
 @onready var bullet = $Sprite2D
-@onready var rayCast = $RayCast2D
 
-func _physics_process(delta: float) -> void:
-	global_position += Vector2(1, 0).rotated(rotation) * speed * delta
-	bullet.position += Vector2(-2, 2).rotated(-rotation)
-	if rayCast.is_colliding():
-		queue_free()
+func _process(delta: float) -> void:
+	position += transform.x * SPEED * delta
