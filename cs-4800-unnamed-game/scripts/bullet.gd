9e10f71a718ae3043286700d89ba93e515abb695
LucMat50 <lmaturino5@gmail.com>
2026-03-31 23:22:42 -0700

    T-T enemy collision was disabled

--- cs-4800-unnamed-game/scripts/bullet.gd
+++ cs-4800-unnamed-game/scripts/bullet.gd
@@ -11,23 +11,21 @@ var movement_vector = Vector2(0, -1)
 @onready var ray = $RayCast2D
 
 func _physics_process(delta: float) -> void:
-	position += transform.x * SPEED * delta
-	var move_amount = SPEED * delta
-	ray.target_position = Vector2(move_amount + 2, 0)
+	global_position += Vector2(1, 0).rotated(rotation) * SPEED * delta
+	#position += transform.x * SPEED * delta
+	ray.target_position = Vector2((SPEED * delta) + 10, 0)
 	ray.force_raycast_update()
 	
 	if ray.is_colliding():
 		var collider = ray.get_collider()
 		enemy_hit(collider)
-	else:
-		position += transform.x * SPEED * delta
 	
-
 func enemy_hit(body: Node2D):
 	print("enemy hit")
 	print(rotation)
 	if body.is_in_group("Enemy"):
 		body.queue_free()
+		queue_free()
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
